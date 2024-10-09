97b62771792a36daad71fc72f2b3659ee37544cc
Julio Rarick <juliorarick@gmail.com>
2024-10-09 20:59:22 -0300

    ✨ feat: add check-in use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,3 +1,5 @@
+import { randomUUID } from 'node:crypto'
+
 import { Prisma, User } from '@prisma/client'
 
 import { UsersRepository } from '../users-repository'
@@ -25,7 +27,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'fake-id',
+      id: randomUUID(),
       email: data.email,
       name: data.name,
       password_hash: data.password_hash,
