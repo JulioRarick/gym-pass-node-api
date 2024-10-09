e94d5be6fd0730161adf06db0c88a467ca281ef8
Julio Rarick <juliorarick@gmail.com>
2024-10-09 20:42:30 -0300

    ✨ feat: add get user profile use-case

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,4 +1,4 @@
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 
 import { prisma } from '@/lib/prisma'
 
@@ -8,6 +8,16 @@ export class PrismaUsersRepository implements UsersRepository {
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
+    })
+
+    return user
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
     })
 
     return user
